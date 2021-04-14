47cec7b08508e79c2f89aac9e16b787a849e7f84
Ahmed Mahfouz <ahmed15.khaled@gmail.com>
2021-04-14 10:50:03 +0200

    changes for new approach

--- clang-tools-extra/clang-tidy/portability/NonPortableIntegerConstantCheck.cpp
+++ clang-tools-extra/clang-tidy/portability/NonPortableIntegerConstantCheck.cpp
@@ -21,20 +21,20 @@ namespace portability {
 /// The mask is problematic if:
 /// - The highest bit is set and everything else are zeroes, for example: `0x80000000`.
 /// - All the bits are set to one, for example: `0xFFFFFFFF`.
-bool isProblematicMask(StringRef HexNumber) {
- // Check whether only the highest bit is set.
-  if (HexNumber[0] == '8' && HexNumber[1] == '0') {
-    // Consume the first non-zero.
-    HexNumber = HexNumber.drop_front();
-    // Consume all the left zeroes.
-    HexNumber = HexNumber.drop_while([](char C) { return C == '0'; });
-    return HexNumber.empty();
-  }
+// bool isProblematicMask(StringRef HexNumber) {
+//  // Check whether only the highest bit is set.
+//   if (HexNumber[0] == '8' && HexNumber[1] == '0') {
+//     // Consume the first non-zero.
+//     HexNumber = HexNumber.drop_front();
+//     // Consume all the left zeroes.
+//     HexNumber = HexNumber.drop_while([](char C) { return C == '0'; });
+//     return HexNumber.empty();
+//   }
 
-  // Check whether all the bits are set to one by consuming all the 'F's.
-  HexNumber = HexNumber.drop_while([](char C) { return C == 'F' || C == 'f'; });
-  return HexNumber.empty();
-}
+//   // Check whether all the bits are set to one by consuming all the 'F's.
+//   HexNumber = HexNumber.drop_while([](char C) { return C == 'F' || C == 'f'; });
+//   return HexNumber.empty();
+// }
 
 void NonPortableIntegerConstantCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(integerLiteral().bind("integer"), this);
@@ -43,25 +43,61 @@ void NonPortableIntegerConstantCheck::registerMatchers(MatchFinder *Finder) {
 void NonPortableIntegerConstantCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedInt = Result.Nodes.getNodeAs<IntegerLiteral>("integer");
 
-  if (MatchedInt) {
-    StringRef MaskStr = Lexer::getSourceText(
-        CharSourceRange::getTokenRange(MatchedInt->getSourceRange()),
-        *Result.SourceManager, Result.Context->getLangOpts(), 0);
+  StringRef LiteralStr = Lexer::getSourceText(CharSourceRange::getTokenRange(MatchedInt->getSourceRange()),
+  *Result.SourceManager, Result.Context->getLangOpts(), nullptr).lower();
 
-    MaskStr = StringRef(MaskStr.lower());
-    if (!MaskStr.consume_front("0x"))
-      return;
 
-    MaskStr = MaskStr.take_while(llvm::isHexDigit);
+  // StringRef MaskStr = Lexer::getSourceText(CharSourceRange::getTokenRange(MatchedInt->getSourceRange()),
+  //     *Result.SourceManager, Result.Context->getLangOpts(), 0);
 
-    if (!isProblematicMask(MaskStr))
-      return;
 
-    diag(MatchedInt->getBeginLoc(),
-         "integer is being used in a non-portable manner");
+  llvm::APInt LiteralValue = MatchedInt->getValue();
+  //  llvm::errs()<< "\n\n MaskStr: " << MaskStr << "\n str: " << LiteralStr << "\n APINT: " << LiteralValue << "\n\n";
+
+  StringRef StrippedLiteral{LiteralStr};
+  // llvm::errs()<< "\n\n" << StrippedLiteral<<"\n";
+  // StrippedLiteral.consume_front("0b");
+  // StrippedLiteral.consume_front("0x");
+  // StrippedLiteral = StrippedLiteral.take_while(llvm::isHexDigit);
+  // llvm::erase_value(StrippedLiteral, '\''); // Skip digit separators.
+
+  // llvm::errs()<< StrippedLiteral << "\n\n";
+
+  // const bool RepresentsZero = LiteralValue.isNullValue();
+  // const bool HasLeadingZeroes = StrippedLiteral[0] == '0';
+  // const bool IntegralPattern = HasLeadingZeroes && !RepresentsZero;
+
+  if (StrippedLiteral.consume_front("0b") || StrippedLiteral.consume_front("0x")){
+    const bool RepresentsZero = LiteralValue.isNullValue();
+    const bool HasLeadingZeroes = StrippedLiteral[0] == '0';
+    const bool IntegralPattern = HasLeadingZeroes && !RepresentsZero;
+
+    if (IntegralPattern)
+      diag(MatchedInt->getBeginLoc(),
+          "integer is being used in a non-portable manner");
   }
+
+
+  // if (MatchedInt) {
+  //   StringRef MaskStr = Lexer::getSourceText(
+  //       CharSourceRange::getTokenRange(MatchedInt->getSourceRange()),
+  //       *Result.SourceManager, Result.Context->getLangOpts(), 0);
+
+  //   MaskStr = StringRef(MaskStr.lower());
+  //   if (!MaskStr.consume_front("0x"))
+  //     return;
+
+  //   MaskStr = MaskStr.take_while(llvm::isHexDigit);
+
+  //   if (!isProblematicMask(MaskStr))
+  //     return;
+
+  //  diag(MatchedInt->getBeginLoc(),
+  //       "integer is being used in a non-portable manner");
+  // }
 }
 
 } // namespace portability
 } // namespace tidy
 } // namespace clang
+
--- clang-tools-extra/test/clang-tidy/checkers/portability-non-portable-integer-constant.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/portability-non-portable-integer-constant.cpp
@@ -1,33 +1,49 @@
 // RUN: %check_clang_tidy %s portability-non-portable-integer-constant %t
 
 // #define ULONG_MAX 0xFFFFFFFFUL
-#include <limits.h>
-#include <stdio.h>
+// #include <limits.h>
+// #include <stdio.h>
 
 const unsigned long mask = 0xFFFFFFFF;
-// CHECK-MESSAGES: :[[@LINE-1]]:28: warning: integer is being used in a non-portable manner
+// CaHECK-MESSAGES: :[[@LINE-1]]:28: warning: integer is being used in a non-portable manner
 
 const unsigned long mask1 = 0xffffffffUL;
-// CHECK-MESSAGES: :[[@LINE-1]]:29: warning: integer is being used in a non-portable manner
+// CaHECK-MESSAGES: :[[@LINE-1]]:29: warning: integer is being used in a non-portable manner
 
 const unsigned long mask2 = 0x80000000;
-// CHECK-MESSAGES: :[[@LINE-1]]:29: warning: integer is being used in a non-portable manner
+// CaHECK-MESSAGES: :[[@LINE-1]]:29: warning: integer is being used in a non-portable manner
 
 int stuff() {
   int mask3 = 0xff;
   // CHECK-MESSAGES: :[[@LINE-1]]:15: warning: integer is being used in a non-portable manner
-  int value = 0x55;
+  // int value = 0x55;
   return mask3;
 }
 
-const unsigned long g = 0xC0;
+
+void a7a(){
+  int s32_bin = 0b01; // warning
+  // CHECK-MESSAGES: :[[@LINE-1]]:17: warning: integer is being used in a non-portable manner
+  int s32_oct = 001;  // warning
+  // CaHECK-MESSAGES: :[[@LINE-1]]:17: warning: integer is being used in a non-portable manner
+  // no-decimal-form
+  int s32_hex = 0x01; // warning
+  // CHECK-MESSAGES: :[[@LINE-1]]:17: warning: integer is being used in a non-portable manner
+
+  s32_bin = 0b0; // no-warning
+  s32_oct = 00;  // no-warning
+  int s32 = 0;   // no-warning
+  s32_hex = 0x0; // no-warning
+
+}
+// const unsigned long g = 0xC0;
 // no warnings
 
-const unsigned long x = -1;
+// const unsigned long x = -1;
 // no warnings
 
-const unsigned long y = ~(ULONG_MAX >> 1);
+// const unsigned long y = ~(ULONG_MAX >> 1);
 // no warnings
 
-const unsigned long z = 0x11111111;
+// const unsigned long z = 0x11111111;
 // no warnings
