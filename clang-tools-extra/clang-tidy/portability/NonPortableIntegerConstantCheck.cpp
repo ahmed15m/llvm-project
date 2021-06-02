0d2b3b7ee062c73759a1c7ed1b5de4f931ff1dc5
Ahmed Mahfouz <ahmed15.khaled@gmail.com>
2021-06-02 10:48:36 +0200

    more fixes

--- clang-tools-extra/clang-tidy/portability/NonPortableIntegerConstantCheck.cpp
+++ clang-tools-extra/clang-tidy/portability/NonPortableIntegerConstantCheck.cpp
@@ -18,23 +18,6 @@ namespace clang {
 namespace tidy {
 namespace portability {
 
-/// The mask is problematic if:
-/// - The highest bit is set and everything else are zeroes, for example: `0x80000000`.
-/// - All the bits are set to one, for example: `0xFFFFFFFF`.
-// bool isProblematicMask(StringRef HexNumber) {
-//  // Check whether only the highest bit is set.
-//   if (HexNumber[0] == '8' && HexNumber[1] == '0') {
-//     // Consume the first non-zero.
-//     HexNumber = HexNumber.drop_front();
-//     // Consume all the left zeroes.
-//     HexNumber = HexNumber.drop_while([](char C) { return C == '0'; });
-//     return HexNumber.empty();
-//   }
-
-//   // Check whether all the bits are set to one by consuming all the 'F's.
-//   HexNumber = HexNumber.drop_while([](char C) { return C == 'F' || C == 'f'; });
-//   return HexNumber.empty();
-// }
 
 void NonPortableIntegerConstantCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(integerLiteral().bind("integer"), this);
@@ -46,55 +29,28 @@ void NonPortableIntegerConstantCheck::check(const MatchFinder::MatchResult &Resu
   StringRef LiteralStr = Lexer::getSourceText(CharSourceRange::getTokenRange(MatchedInt->getSourceRange()),
   *Result.SourceManager, Result.Context->getLangOpts(), nullptr).lower();
 
-
-  // StringRef MaskStr = Lexer::getSourceText(CharSourceRange::getTokenRange(MatchedInt->getSourceRange()),
-  //     *Result.SourceManager, Result.Context->getLangOpts(), 0);
-
-
   llvm::APInt LiteralValue = MatchedInt->getValue();
-  //  llvm::errs()<< "\n\n MaskStr: " << MaskStr << "\n str: " << LiteralStr << "\n APINT: " << LiteralValue << "\n\n";
 
   StringRef StrippedLiteral{LiteralStr};
-  // llvm::errs()<< "\n\n" << StrippedLiteral<<"\n";
-  // StrippedLiteral.consume_front("0b");
-  // StrippedLiteral.consume_front("0x");
-  // StrippedLiteral = StrippedLiteral.take_while(llvm::isHexDigit);
-  // llvm::erase_value(StrippedLiteral, '\''); // Skip digit separators.
+  StrippedLiteral.consume_front("0b");
+  StrippedLiteral.consume_front("0x");
+  StrippedLiteral = StrippedLiteral.take_while(llvm::isHexDigit);
 
-  // llvm::errs()<< StrippedLiteral << "\n\n";
+  llvm::errs()<< "\n str: " << LiteralStr << "\n APINT: " << LiteralValue << "  stripped:" << StrippedLiteral <<"  size:"<< LiteralValue.isAllOnesValue() << "\n\n";
 
-  // const bool RepresentsZero = LiteralValue.isNullValue();
-  // const bool HasLeadingZeroes = StrippedLiteral[0] == '0';
-  // const bool IntegralPattern = HasLeadingZeroes && !RepresentsZero;
+  if (StrippedLiteral.empty())
+    return;
 
-  if (StrippedLiteral.consume_front("0b") || StrippedLiteral.consume_front("0x")){
-    const bool RepresentsZero = LiteralValue.isNullValue();
-    const bool HasLeadingZeroes = StrippedLiteral[0] == '0';
-    const bool IntegralPattern = HasLeadingZeroes && !RepresentsZero;
+  const bool RepresentsZero = LiteralValue.isNullValue();
+  const bool HasLeadingZeroes = StrippedLiteral[0] == '0';
+  const bool AllBitsAreSet = LiteralValue.isAllOnesValue();
+  const bool IntegralPattern = (HasLeadingZeroes && !RepresentsZero) || AllBitsAreSet;
 
-    if (IntegralPattern)
-      diag(MatchedInt->getBeginLoc(),
-          "integer is being used in a non-portable manner");
-  }
+  if (IntegralPattern)
+    diag(MatchedInt->getBeginLoc(),
+        "integer is being used in a non-portable manner");
 
 
-  // if (MatchedInt) {
-  //   StringRef MaskStr = Lexer::getSourceText(
-  //       CharSourceRange::getTokenRange(MatchedInt->getSourceRange()),
-  //       *Result.SourceManager, Result.Context->getLangOpts(), 0);
-
-  //   MaskStr = StringRef(MaskStr.lower());
-  //   if (!MaskStr.consume_front("0x"))
-  //     return;
-
-  //   MaskStr = MaskStr.take_while(llvm::isHexDigit);
-
-  //   if (!isProblematicMask(MaskStr))
-  //     return;
-
-  //  diag(MatchedInt->getBeginLoc(),
-  //       "integer is being used in a non-portable manner");
-  // }
 }
 
 } // namespace portability
--- clang-tools-extra/test/clang-tidy/checkers/portability-non-portable-integer-constant.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/portability-non-portable-integer-constant.cpp
@@ -1,31 +1,31 @@
 // RUN: %check_clang_tidy %s portability-non-portable-integer-constant %t
 
-// #define ULONG_MAX 0xFFFFFFFFUL
+#define ULONG_MAX 0xFFFFFFFFUL
 // #include <limits.h>
 // #include <stdio.h>
 
 const unsigned long mask = 0xFFFFFFFF;
-// CaHECK-MESSAGES: :[[@LINE-1]]:28: warning: integer is being used in a non-portable manner
+// CHECK-MESSAGES: :[[@LINE-1]]:28: warning: integer is being used in a non-portable manner
 
-const unsigned long mask1 = 0xffffffffUL;
-// CaHECK-MESSAGES: :[[@LINE-1]]:29: warning: integer is being used in a non-portable manner
+const unsigned long mask1 = 0xffffffffUL; // not matching it as not all bits are set for UL, but not sure?
+// --CHECK-MESSAGES: :[[@LINE-1]]:29: warning: integer is being used in a non-portable manner
 
 const unsigned long mask2 = 0x80000000;
-// CaHECK-MESSAGES: :[[@LINE-1]]:29: warning: integer is being used in a non-portable manner
+// --CHECK-MESSAGES: :[[@LINE-1]]:29: warning: integer is being used in a non-portable manner
 
 int stuff() {
   int mask3 = 0xff;
-  // CHECK-MESSAGES: :[[@LINE-1]]:15: warning: integer is being used in a non-portable manner
+  // --CHECK-MESSAGES: :[[@LINE-1]]:15: warning: integer is being used in a non-portable manner
   // int value = 0x55;
   return mask3;
 }
 
 
-void a7a(){
+void test(){
   int s32_bin = 0b01; // warning
   // CHECK-MESSAGES: :[[@LINE-1]]:17: warning: integer is being used in a non-portable manner
   int s32_oct = 001;  // warning
-  // CaHECK-MESSAGES: :[[@LINE-1]]:17: warning: integer is being used in a non-portable manner
+  // CHECK-MESSAGES: :[[@LINE-1]]:17: warning: integer is being used in a non-portable manner
   // no-decimal-form
   int s32_hex = 0x01; // warning
   // CHECK-MESSAGES: :[[@LINE-1]]:17: warning: integer is being used in a non-portable manner
@@ -35,15 +35,19 @@ void a7a(){
   int s32 = 0;   // no-warning
   s32_hex = 0x0; // no-warning
 
+  // s32 = 0b1; // no-warning
+  // s32 = 01;  // no-warning
+  // s32 = 1;   // no-warning
+  // s32 = 0x1; // no-warning
 }
-// const unsigned long g = 0xC0;
+const unsigned long g = 0xC0;
 // no warnings
 
-// const unsigned long x = -1;
+const unsigned long x = -1;
 // no warnings
 
-// const unsigned long y = ~(ULONG_MAX >> 1);
+const unsigned long y = ~(ULONG_MAX >> 1);
 // no warnings
 
-// const unsigned long z = 0x11111111;
+const unsigned long z = 0x11111111;
 // no warnings
